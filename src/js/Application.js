9cba4e83ed5c005473b9d7efbe084bc36e7f3f71
Milen <milenmihalkov@gmail.com>
2023-01-17 20:00:31 +0200

    Planets

--- src/js/Application.js
+++ src/js/Application.js
@@ -6,32 +6,69 @@ export default class Application extends EventEmitter {
       READY: "ready",
     };
   }
-
+  
   constructor() {
     super();
 
-    const _loading = document.getElementById('progress');
-    let apiUrl  ='https://swapi.boom.dev/api/planets?page=2';
-    this._load();
-
+    this._create();
+    this._startLoading();
     this.emit(Application.events.READY);
   }
 
    async _load(){
-    let response = await fetch(this.apiUrl);
+    const apiUrl  ='https://swapi.boom.dev/api/planets';
 
+    let response = await fetch(apiUrl);
     console.log(response.status); // 200
     console.log(response.statusText); // OK
 
     if (response.status === 200) {
+      
         let data = await response.json();
-        console.log(data);
+ 
+          return data;
+
+  
     }
   }
 
-  _create(){}
-  _startLoading(){}
-  _stopLoading(){}
+  _create(){
+    this._load().then((response) => { 
+      console.log(response.results);
+      response.results.forEach((element) => {
 
-  
+        let block = document.createElement("section");
+        block.classList.add("planetscontainer");
+    
+        block.innerHTML = this._render({
+          name: element.name,
+          rotation_period: element.rotation_period,
+          diameter: element.diameter,
+        });
+
+        this._stopLoading();
+        document.body.querySelector(".content-wrapper").appendChild(block);
+      });
+    });
+  }
+  _startLoading(){
+    const progressBar = document.getElementById('progress');
+    progressBar.style.display = 'block';
+  }
+  _stopLoading(){
+    const progressBar = document.getElementById('progress');
+    progressBar.style.display = 'none';
+  }
+
+  _render({ name, rotation_period, diameter }) {
+    return `<article class="content">
+    <h6>${name}</h6>
+    <div> 
+      <ul>
+        <li>Rotation Period: <strong>${rotation_period}</strong> units</li>
+        <li>Diameter: <strong>${diameter}</strong> units</li>
+      </ul>
+    </div>
+    <article>`;
+  }
 }
