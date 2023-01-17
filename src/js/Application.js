8ebdd4a0cea9cbd804b56c7f7b144b2a94767228
Milen <milenmihalkov@gmail.com>
2023-01-17 10:09:02 +0200

    Runtime error fixed
    Runtime error fixed

--- src/js/Application.js
+++ src/js/Application.js
@@ -10,11 +10,28 @@ export default class Application extends EventEmitter {
   constructor() {
     super();
 
-    const button = document.querySelector(".button");
-    button.addEventListener("click", () => {
-      alert("💣");
-    });
+    const _loading = document.getElementById('progress');
+    let apiUrl  ='https://swapi.boom.dev/api/planets?page=2';
+    this._load();
 
     this.emit(Application.events.READY);
   }
+
+   async _load(){
+    let response = await fetch(this.apiUrl);
+
+    console.log(response.status); // 200
+    console.log(response.statusText); // OK
+
+    if (response.status === 200) {
+        let data = await response.json();
+        console.log(data);
+    }
+  }
+
+  _create(){}
+  _startLoading(){}
+  _stopLoading(){}
+
+  
 }
